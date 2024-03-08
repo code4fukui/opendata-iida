28e1decbbad03e65e64b72c4e518da2c5a126c24
Taisuke Fukuno <fukuno@jig.jp>
2024-03-09 07:40:23 +0900

    add jismesh-es, mix data

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # opendata-iida
  
+- [オープンデータ飯田マップ](https://code4fukui.github.io/opendata-iida/)
+- [文化財、子育て施設、個別表示マップ](https://code4fukui.github.io/opendata-iida/map.html)
--- download.js
+++ download.js
@@ -1,12 +1,31 @@
 import { CSV } from "https://js.sabae.cc/CSV.js";
+import jismesh from "https://code4fukui.github.io/jismesh-es/index.js";
 
-const urls = [
-  "https://www.city.iida.lg.jp/uploaded/attachment/67233.csv",
-  "https://www.city.iida.lg.jp/uploaded/attachment/67211.csv",
+const dataset = [
+  {
+    url: "https://www.city.iida.lg.jp/uploaded/attachment/67233.csv",
+    color: "red",
+  },
+  {
+    url:  "https://www.city.iida.lg.jp/uploaded/attachment/67211.csv",
+    color: "green",
+  },
 ];
 
-for (const url of urls) {
+const list = [];
+for (const d of dataset) {
+  const url = d.url;
   const data = await CSV.fetchJSON(url);
   const fn = url.substring(url.lastIndexOf("/") + 1);
   await Deno.writeTextFile(fn, CSV.stringify(data));
+
+  const color = d.color;
+  data.forEach(i => {
+    i.color = color;
+    if (i.緯度 && i.経度) {
+      i.jismesh3 = jismesh.toMeshCode(i.緯度, i.経度, 3);
+    }
+    list.push(i);
+  });
 }
+await Deno.writeTextFile("iida.csv", CSV.stringify(list));
